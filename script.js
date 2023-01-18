c4ce49ad6c8161dd29d7020726338188c5aeaf01
Styven Noronha <styvenn16@gmail.com>
2023-01-18 20:05:43 -0300

    terceiro

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("ola mundo")
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["01-01", "01-20", "01-06"],
+  food:["01-01","01-18","01-21"],
+  water:["01-19"]
+}
+
+nlwSetup.addDay('18/01')
+nlwSetup.addDay("19/01")
+nlwSetup.addDay("20/01")
+nlwSetup.addDay("21/01")
+nlwSetup.addDay("21/01")
+
+nlwSetup.setData(data)
+nlwSetup.load()
